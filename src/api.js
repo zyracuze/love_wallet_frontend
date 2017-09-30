9d12610caff9eb81773ebd2a84e5b9c062c271f1
dumpsayamrat <sayamrat.kt@gmail.com>
2017-09-30 16:38:02 +0700

    Edit endpoint

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8882/api';
+const BASE_URL = 'http://139.59.106.112:3000/api';
 
 export const getHello = () => axios.get(`${BASE_URL}/hello`);
